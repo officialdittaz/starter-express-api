76ecdfe9a1581c3529f7cb799b83092b3fc2b4e1
DITTAZ <86673643+officialdittaz@users.noreply.github.com>
2024-02-14 03:32:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+  let html = fs.readFileSync('./index.html', 'utf-8')
+  res.end(html)
 })
 app.listen(process.env.PORT || 3000)
